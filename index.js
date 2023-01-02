9927d4bf905d4645eaf4918b756b61647fd19640
bmj <bmj44571627@gmail.com>
2023-01-02 15:39:07 +0900

    mongodb data 연동

--- index.js
+++ index.js
@@ -4,16 +4,19 @@ const path = require("path");
 const mongoose = require("mongoose");
 const { Todo } = require("./model/TodoModel");
 
-const app = express();
-const port = 5000;
+const config = require("./config/key.js");
 
+const app = express();
+
+const port = 5000;
+//요청이 들어오면 json 사용 및 url 인코딩 진행해줌
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "../client/build/")));
 
 app.listen(port, () => {
   mongoose
-    .connect(
-      "mongodb+srv://admin:q1q2q3q4@cluster0.edebv8n.mongodb.net/todoData?retryWrites=true&w=majority"
-    )
+    .connect(config.mongoURI)
     .then(() => {
       console.log("DB 연결 성공");
       console.log(`Example app listening on port ${port}`);
@@ -33,22 +36,7 @@ app.get("*", (request, response) => {
   response.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.post("/api/test", (request, response) => {
-  console.log(request.body);
-  response.status(200).json({
-    success: true,
-    text: request.body.message,
-    name: request.body.name,
-  });
-});
-
-//등록
-
 app.post("/api/post/submit", (request, response) => {
-  // console.log(request.body)
   let temp = request.body;
   const todoPost = new Todo(temp);
   todoPost
@@ -59,5 +47,20 @@ app.post("/api/post/submit", (request, response) => {
     .catch((error) => {
       console.log(err);
       response.status(400).json({ success: false });
+    });
+});
+
+//목록 읽어오기
+app.post("/api/post/list", (request, response) => {
+  console.log("전체목록 호출");
+  Todo.find({})
+    .exec()
+    .then((doc) => {
+      console.log(doc);
+      response.status(200).json({success:true, initTodo:doc});
+    })
+    .catch((error) => {
+      console.log(error);
+      response.status(400).json({success:true});
     });
 });
